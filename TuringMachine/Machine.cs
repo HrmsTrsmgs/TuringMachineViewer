cd152febb524516bfa7651c81883cd5edb180d61
NITTA Mitsuhiro <m_nitta@ap-com.co.jp>
2017-06-12 14:32:07 +0900

    リファクタリング

---
+++ TuringMachine/Machine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Marimo.TuringMachineViewer.TuringMachine
+{
+    public class Machine
+    {
+        public string State { get; private set; }
+
+        public Tape Tape { get; }
+
+        public Machine(string blankSymble, IEnumerable<string> tape, params (string CurrentState, string ScannedSymbol, string PrintSymbol, Direction MoveTape, string NextState)[] table)
+        {
+            State = "1";
+
+            Tape = new Tape(blankSymble, tape);
+            this.table = table.ToDictionary(x => (x.CurrentState, x.ScannedSymbol), x => (x.PrintSymbol, x.MoveTape, x.NextState));
+        }
+        private Dictionary<(string CurrentState, string ScannedSymbol), (string PrintSymbol, Direction MoveTape, string NextState)> table { get; }
+
+        public IEnumerable<(string CurrentState, string ScannedSymbol, string PrintSymbol, Direction MoveTape, string NextState)> Table
+        {
+            get
+            {
+                foreach(var kv in table)
+                {
+                    yield return (kv.Key.CurrentState, kv.Key.ScannedSymbol, kv.Value.PrintSymbol, kv.Value.MoveTape, kv.Value.NextState);
+                }
+            }
+        }
+        
+        public bool MoveNext()
+        {
+            var key = (State, Tape.Current);
+            if (table.ContainsKey(key))
+            {
+                var next = table[key];
+                Tape.Current = next.PrintSymbol;
+                State = next.NextState;
+                Tape.Move(next.MoveTape);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            var key = (State, Tape.Current);
+            if (table.ContainsKey(key))
+            {
+                return table[key].ToString();
+            }
+            return "End";
+        }
+    }
+}
---
+++ TuringMachine/Tape.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Marimo.TuringMachineViewer.TuringMachine
+{
+    public class Tape
+    {
+        public Tape(string blank, IEnumerable<string> init)
+        {
+            Blank = blank;
+            plusStates.AddRange(init);
+        }
+
+        public string Blank { get; private set; }
+
+        private int CurrentIndex { get; set; } = 0;
+
+        public int Position => CurrentIndex + minusStates.Count;
+
+        public void Move(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Left:
+                    CurrentIndex--;
+                    break;
+                case Direction.Right:
+                    CurrentIndex++;
+                    break;
+            }
+            var d = this[CurrentIndex];
+        }
+
+        private List<string> plusStates = new List<string>();
+        private List<string> minusStates = new List<string>();
+
+        private string this[int index]
+        {
+            get
+            {
+                if (0 <= index)
+                {
+                    while (plusStates.Count <= index)
+                    {
+                        plusStates.Add(Blank);
+                    }
+                    return plusStates[index];
+                }
+                else
+                {
+                    var listIndex = -index - 1;
+
+                    while (minusStates.Count <= listIndex)
+                    {
+                        minusStates.Add(Blank);
+                    }
+                    return minusStates[listIndex];
+                }
+            }
+            set
+            {
+                if (0 <= index)
+                {
+                    while (plusStates.Count <= index)
+                    {
+                        plusStates.Add(Blank);
+                    }
+                    plusStates[index] = value;
+                }
+                else
+                {
+                    var listIndex = -index - 1;
+
+                    while (minusStates.Count <= listIndex)
+                    {
+                        minusStates.Add(Blank);
+                    }
+                    minusStates[listIndex] = value;
+                }
+            }
+        }
+
+        public string Current
+        {
+            get
+            {
+                return this[CurrentIndex];
+            }
+            set
+            {
+                this[CurrentIndex] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join("", minusStates.Reverse<string>().Concat(plusStates));
+        }
+    }
+}
